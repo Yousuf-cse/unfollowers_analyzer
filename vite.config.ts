5c7fac4709f8e55aa6447dd6d92aae9100b33add
Yousuf-cse <yousuf.mallik79@gmail.com>
2026-01-30 21:37:50 +0530

    feat: -add vite-plugin-static-copy  for auto-coping the manifest.json and icons in dist folder. - add dummy placeholders png icons (will replace it later)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "manifest.json",
+          dest: ".",
+        },
+        {
+          src: "icons",
+          dest: "icons",
+        },
+      ],
+    }),
+  ],
+});
